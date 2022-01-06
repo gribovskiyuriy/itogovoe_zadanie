b4795f2a7db7b41bb03b48ff7b9977e56fccfa2e
gribovskiy_uriy <romantir13@gmail.com>
2022-01-06 20:40:02 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+// //что делаем?
+
+// //Задать массив
+int[] A = new int[100];
+
+    int index = 0;
+    while (index < 10)
+    {
+       A[index] = new Random().Next(-50,50);
+        index++;
+    }
+
+    index = 0;
+    //Заполняем массив
+        while (index < 10)
+    {
+       Console.WriteLine(A[index]);
+        index++;
+    }
+string valueNumber (int i)
+{
+    string res = string.Empty;
+    if (i % 2 == 0) {res = $"Чётное";}
+    else 
+    return res;
+}
+string res = valueNumber(13);
+Console.WriteLine($"{res}");
